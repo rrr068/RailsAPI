990b370746be28bc7cf223a60e3be72267a038f9
山田燎弥 <ryoya068@yamadaniwabiwatarunoMacBook-Air.local>
2024-03-02 23:11:33 +0900

    development api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
-        include DeviseHackFakeSession
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseHackFakeSession
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
